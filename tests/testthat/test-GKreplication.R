8579113f0ec5aa17a39f123d71a595aaad23919c
angusmoore <angus.moore@internode.on.net>
2017-09-20 12:35:02 +1000

    Fix typo in test-GKreplication.R

--- tests/testthat/test-GKreplication.R
+++ tests/testthat/test-GKreplication.R
@@ -6,4 +6,4 @@ gkvar <- VAR(GKdata[, c("logip", "logcpi", "gs1", "ebp")], p = 12, type = "const
 shockcol <- externalinstrument(gkvar, GKdata$ff4_tc, "gs1")
 
 shouldbe <- c(0.04577746, -0.03861414, 0.21901224, 0.11187556)
-test_that(shock, equals(shouldbe))
+test_that(shockcol, equals(shouldbe))
