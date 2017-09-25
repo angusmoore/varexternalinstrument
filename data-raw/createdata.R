2d033def35940a8cd1ac80a581b387df85e42432
Angus <angus.moore@internode.on.net>
2017-09-25 13:53:27 +1000

    Changed data to use same estimation sample as GK. Updated tests accordingly. Should now exactly match GK's results.

--- data-raw/createdata.R
+++ data-raw/createdata.R
@@ -1,3 +1,4 @@
 GKdata <- read.csv("GKdata.csv", header = TRUE)
-
+# zero out the 1990 year of data because GK use 1991 onwards as their sample
+GKdata[127:138,"ff4_tc"] <- NA
 devtools::use_data(GKdata, overwrite = TRUE)
--- tests/testthat/test-GKreplication.R
+++ tests/testthat/test-GKreplication.R
@@ -5,5 +5,5 @@ data(GKdata)
 gkvar <- VAR(GKdata[, c("logip", "logcpi", "gs1", "ebp")], p = 12, type = "const")
 shockcol <- externalinstrument(gkvar, GKdata$ff4_tc, "gs1")
 
-shouldbe <- c(0.04577746, -0.03861414, 0.21901224, 0.11187556)
+shouldbe <- c(0.02886238, -0.03275585, 0.22507245, 0.11296773 )
 test_that(shockcol, equals(shouldbe))
