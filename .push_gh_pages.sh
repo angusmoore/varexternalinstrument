80e3ea4fdc85ec77d9a95406e670586f18389551
Angus <angus.moore@internode.on.net>
2017-10-12 07:01:02 +1100

    Add script to auto build and deploy docs to gh-pages.

---
+++ .push_gh_pages.sh
@@ -0,0 +1,22 @@
+R -e "pack <- 'varexternalinstrument'; path <- find.package(pack); system(paste(shQuote(file.path(R.home('bin'), 'R')), 'CMD', 'Rd2pdf', shQuote(path)))"
+
+rm -rf out || exit 0;
+mkdir out;
+
+GH_REPO="@github.com/angusmoore/varexternalinstrument.git"
+
+FULL_REPO="https://$GH_TOKEN$GH_REPO"
+
+for files in '*.tar.gz'; do
+        tar xfz $files
+done
+
+cd out
+git init
+git config user.name "travis"
+git config user.email "travis"
+cp ../varexternalinstrument.pdf varexternalinstrument.pdf
+
+git add .
+git commit -m "Rebuilt PDF package documentation and deployed to gh-pages"
+git push --force --quiet $FULL_REPO master:gh-pages
